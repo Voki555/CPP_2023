cab65532cf6d264f15057977bd7eff337e77a395
Voki555 <125806037+Voki555@users.noreply.github.com>
2023-03-01 17:15:56 +0800

    Add files via upload

---
+++ Task_1_S11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Перебор чисел от 0 до 100
+
+int main(){
+    int a = 0;
+    while(a<=100){
+    if (a % 3 == 0){
+        cout << a << '\n';
+        a++;
+    } else ;{
+        a++;
+    }
+    }
+ return 0;
+}
+
