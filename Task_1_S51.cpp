9539bfa81efe65b88055d5da2fc2127542a176cb
Voki555 <125806037+Voki555@users.noreply.github.com>
2023-03-01 22:28:23 +0800

    Add files via upload

---
+++ Task_1_S51.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    string text;
+    cout << "Enter the text" << '\n';
+    cin >> text;
+    reverse(text.begin(), text.end());
+    cout << text <<"\n"; // 324
+}
