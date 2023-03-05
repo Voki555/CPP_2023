1dd64db4a89563ef13ec4c90c24487fd2e5db018
Voki555 <125806037+Voki555@users.noreply.github.com>
2023-03-05 21:28:39 +0800

    Add files via upload

---
+++ Task_1_S42.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+int sorti(int number[],int n){
+    for(int i=1;i<n;i++)
+    {
+        int j = i-1;//0
+        while(j>=0 && number[j]<number[j+1])
+        {
+            swap(number[j],number[j+1]);
+            j--;
+        }
+    }
+}
+
+
+int main(){
+    setlocale(LC_ALL,"Russian");
+    int i=0,n,num;
+    cout << "¬ведите длину массива:" << '\n';
+    cin >> n ;
+    int number[n];
+    while(i<=n-1){
+        for(i;i<=n-1;i++){
+        cout << "¬ведите " << i+1 << " число:" <<'\n';
+        cin >> num;
+        number[i] = num;
+        }
+    }
+    sorti(number,n);
+
+    cout << "”спешно отсортирован:" << endl;
+	for(int i = 0; i < n; i++)
+		cout << number[i] << " ";
+    return 0;
+}
