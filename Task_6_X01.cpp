e27ef0c202133c4d5a6aedc36e1e661b83a33216
Voki555 <125806037+Voki555@users.noreply.github.com>
2023-03-19 13:49:08 +0800

    Add files via upload

--- Task_6_X01.cpp
+++ Task_6_X01.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
-int queen_hit(string f, string p);
+bool queen_hit(string f, string p){
+    int f1 = f[0],p1 = p[0],f2 = f[1], p2 = p[1];
+    if(f1<96 || f1>105 || f2<=48 || f2>=57 || p1<96 || p1>105 || p2<=48 || p2>=57 || f1 == p1 & f[1] == p[1]){
+        cout << "Error, " << 0;
+        return false;
+    }
+    else if(f1 == p1 || f[1] == p[1] || abs(f1-p1) == abs(f[1]-p[1])){
+        cout << 1 << endl;
+        return true;
+    }
+    else{
+        cout << 0 << endl;
+        return false;
+    }
+}
+
 
 int main(){
     setlocale(0,"Russian");
     string f,p;
-    bool result;
 
     cout << "Введите положение ферзя(Начиная с буквы):";
     cin >> f;
@@ -21,20 +34,4 @@ int main(){
     return 0;
 }
 
-int queen_hit(string f, string p){
-    bool result;
-    int f1 = f[0],p1 = p[0],f2 = f[1], p2 = p[1];
-    if(f1<96 || f1>105 || f2<=48 || f2>=57 || p1<96 || p1>105 || p2<=48 || p2>=57 || f1 == p1 & f[1] == p[1]){
-        cout << "Error, " << 0;
-        result = false;
-    }
-    else if(f1 == p1 || f[1] == p[1] || f1-p1 == f[1]-p[1] || -(f1-p1) == f[1]-p[1] || f1-p1 == -(f[1]-p[1])){
-        cout << 1 << endl;
-        result = true;
-    }
-    else{
-        result = false;
-        cout << 0 << endl;
-    }
-return result;
-}
+
